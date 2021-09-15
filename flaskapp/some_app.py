8f2e8a98eeb2f23ae1b2dd08938b7ff9fabaebb0
Igor Trofimov <igor.trofimov@t-systems.com>
2021-09-15 18:47:45 +0300

    changed client and server parts

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,5 +1,5 @@
-print("Hello world")
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,6 +8,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return " <html><head></head> <body> Hello World! </body></html>"
+
+
+# наша новая функция сайта
+@app.route("/data_to")
+def data_to():
+    # создаем переменные с данными для передачи в шаблон
+    some_pars = {'user': 'Ivan', 'color': 'red'}
+    some_str = 'Hello my dear friends!'
+    some_value = 10
+    # передаем данные в шаблон и вызываем его
+    return render_template('simple.html', some_str=some_str,
+                           some_value=some_value, some_pars=some_pars)
 
 
 if __name__ == "__main__":
