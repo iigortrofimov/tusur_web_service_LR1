cf339b7525b50b30a1449f38763b8f3b334e6565
Igor Trofimov <igor.trofimov@t-systems.com>
2021-10-03 14:13:17 +0300

    code style fixed and added comments

--- flaskapp/color_recogniser.py
+++ flaskapp/color_recogniser.py
@@ -5,17 +5,21 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 
+# Основная функия цветового анализа изображения
+# В качестве аргумента принимает путь до изображения
 def analyse_color(image_path):
-#    print(image_path)
-
+    # считывает изображение
     image = cv2.imread(image_path)
+    # конвертирует формат изображения из BGR в RGB
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     plt.imshow(image)
-
     modified_image = prep_image(image)
     color_analysis(modified_image, image_path)
 
 
+# Функция конвертации из RGB формата в HEX цветовой формат,:
+# вместо трех различных значений (красный, зеленный, синий) будет возвращено
+# одно HEX значение.
 def rgb_to_hex(rgb_color):
     hex_color = "#"
     for i in rgb_color:
@@ -24,22 +28,34 @@ def rgb_to_hex(rgb_color):
     return hex_color
 
 
+# Функция выполняющая преподготовку изображения:
+# изменяет размер и форму. Измение размера опционально,
+# но измение формы обязательтно для проведения цветового анализа.
+# Возвращает измененное изображение.
 def prep_image(raw_img):
     modified_img = cv2.resize(raw_img, (900, 600), interpolation=cv2.INTER_AREA)
     modified_img = modified_img.reshape(modified_img.shape[0] * modified_img.shape[1], 3)
     return modified_img
 
 
+# Функия цветового анализа:
 def color_analysis(img, title: str):
+    # KMeans() для кластеризации верхних цветов.
+    # Внутри функции передаем значение того, на сколько кластеров мы хотим разделить наш анализ
     clf = KMeans(n_clusters=5)
+    # После кластеризации прогнозируем, какие цвета будут иметь наибольший "вес",
+    # то есть те, что занимают наибольшую площадь изображения.
     color_labels = clf.fit_predict(img)
     center_colors = clf.cluster_centers_
+
+    # Counter создает контейнер для элементов в ключах словаря, а их объем сохраняется в виде значений словаря.
     counts = Counter(color_labels)
     ordered_colors = [center_colors[i] for i in counts.keys()]
+
+    # переводим в HEX значения
     hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+    # визуализация результата
     plt.figure(figsize=(12, 8))
     plt.pie(counts.values(), labels=hex_colors, colors=hex_colors)
-
-  #  print('./static/uploads/analyse_' + title.split("/")[3])
+    # сохранение резульата в папку uploads
     plt.savefig('./static/uploads/analyse_' + title.split("/")[3])
-  #  print(hex_colors)
--- flaskapp/lr2_app.py
+++ flaskapp/lr2_app.py
@@ -4,7 +4,6 @@ import string
 
 from PIL import Image
 from flask import Flask, flash, request, redirect, url_for, render_template
-from flask_recaptcha import ReCaptcha
 from flask_wtf import FlaskForm, RecaptchaField
 from werkzeug.utils import secure_filename
 
@@ -14,6 +13,7 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = './static/uploads/'
 
+# сгенерированное значение секретного ключа в 24 символа
 app.secret_key = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(24))
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -24,37 +24,43 @@ app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdiKukUAAAAAGOH6Wve-LnDOwd5AyoGFSf4mRzm'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdiKukUAAAAAG16BmzRIq51tWhmQKRQ281q9wOt'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'custom'}
 
-recaptcha = ReCaptcha(app)
-
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 
+# класс с формой для капчи
 class NetForm(FlaskForm):
     recaptcha = RecaptchaField()
 
 
+# Функция проверки допустимого формата защруженного файла (изображения)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+# Функция-контроллер на GET метод, возвращающий страницу index.html
 @app.route('/')
 def upload_form():
+    # создание формы с капчей
     form = NetForm()
     return render_template('index.html', form=form)
 
 
+# Функция-контроллер на POST метод
 @app.route('/', methods=['POST'])
 def upload_file():
+    # создание формы с капчей
     form = NetForm()
     if request.method == 'POST':
-        # check if the post request has the files part
+        # проверяет содержит запрос файлы или нет
         if 'files[]' not in request.files:
             flash('Файлы не найдены')
             return redirect(request.url)
         files = request.files.getlist('files[]')
+        # проверка на количество файлов
         if len(files) != 2:
             flash('Загрузите 2 изображения')
             return redirect(request.url)
+        # проверка форматов и сохранение изображений
         for file in files:
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
@@ -63,15 +69,18 @@ def upload_file():
 
         final_filename = ''
 
+        # в зависимости от параметра, переданного пользователем выполняется способ склеивания изображения
         if request.form.get('mergetype').__eq__('Вертикальный'):
             final_filename = merge_vertical(files[0].filename, files[1].filename)
         elif request.form.get('mergetype').__eq__('Горизонтальный'):
             final_filename = merge_horizontal(files[0].filename, files[1].filename)
 
+        # вызов функций цветового анализа для исходных и склееного изображений
         color_recogniser.analyse_color("./static/uploads/" + final_filename)
         color_recogniser.analyse_color("./static/uploads/" + files[0].filename)
         color_recogniser.analyse_color("./static/uploads/" + files[1].filename)
 
+        # возврат страницы index.html с адресами всех изображений
         return render_template('index.html', filename=final_filename,
                                color_analyse1='analyse_' + files[0].filename,
                                color_analyse2='analyse_' + files[1].filename,
@@ -79,52 +88,66 @@ def upload_file():
                                form=form)
 
 
+# Функция отображения изображения
 @app.route('/display/<filename>')
 def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
+# Функция склеивания изображения по вертикали
 def merge_vertical(file_name_1, file_name_2):
     file1_path = UPLOAD_FOLDER + file_name_1
     file2_path = UPLOAD_FOLDER + file_name_2
     images = [Image.open(x) for x in [file1_path, file2_path]]
     widths, heights = zip(*(i.size for i in images))
 
+    # Установка высоты и ширины итогового изображения
     total_width = max(widths)
     max_height = sum(heights)
 
+    # формат и размеры нового изображения
     new_im = Image.new('RGB', (total_width, max_height))
 
+    # склеивание изображений по вертикали (y)
     y_offset = 0
     for im in images:
         new_im.paste(im, (0, y_offset))
         y_offset += im.size[1]
 
     result_filename = UPLOAD_FOLDER + 'vert-' + file_name_1.rpartition('.')[0] + '-' + file_name_2
+    # сохранение нового изображений по переданному адресу (названию)
     new_im.save(result_filename)
+    # возврат пути до изображения-результата
     return 'vert-' + file_name_1.rpartition('.')[0] + '-' + file_name_2
 
 
+# Функция склеивания изображения по горизонтали
 def merge_horizontal(file_name_1, file_name_2):
     file1_path = UPLOAD_FOLDER + file_name_1
     file2_path = UPLOAD_FOLDER + file_name_2
     images = [Image.open(x) for x in [file1_path, file2_path]]
     widths, heights = zip(*(i.size for i in images))
 
+    # Установка высоты и ширины итогового изображения
     total_width = sum(widths)
     max_height = max(heights)
 
+    # формат и размеры нового изображения
     new_im = Image.new('RGB', (total_width, max_height))
 
+    # склеивание изображений по горизонтали (x)
     x_offset = 0
     for im in images:
         new_im.paste(im, (x_offset, 0))
         x_offset += im.size[0]
 
     result_filename = UPLOAD_FOLDER + 'horiz-' + file_name_1.rpartition('.')[0] + '-' + file_name_2
+    # сохранение нового изображений по переданному адресу (названию)
     new_im.save(result_filename)
+    # возврат пути до изображения-результата
     return 'horiz-' + file_name_1.rpartition('.')[0] + '-' + file_name_2
 
 
+# Запуск приложения
 if __name__ == '__main__':
     app.run()
